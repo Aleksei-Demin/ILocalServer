863a229e588b3e40d30997976f697a740562fcaa
Aleksei <dr.alone@yandex.ru>
2024-09-20 02:42:47 +0300

    updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,10 +40,12 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    // Добавляем зависимость для NanoHTTPD
+    // Добавляем зависимости
     implementation("org.nanohttpd:nanohttpd:2.3.1")
     implementation ("androidx.localbroadcastmanager:localbroadcastmanager:1.0.0")
     implementation ("androidx.appcompat:appcompat:1.3.0")
+    implementation ("androidx.work:work-runtime-ktx:2.7.1")
+    implementation ("com.squareup.okhttp3:okhttp:4.9.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/v1v3r/infolocalserver/MainActivity.kt
+++ app/src/main/java/com/v1v3r/infolocalserver/MainActivity.kt
@@ -1,32 +1,36 @@
 package com.v1v3r.infolocalserver
 
-import android.accessibilityservice.AccessibilityService
-import android.view.accessibility.AccessibilityManager
-import android.accessibilityservice.AccessibilityServiceInfo
 import android.content.BroadcastReceiver
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import android.provider.Settings
-import android.text.TextUtils
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
 import java.net.Inet4Address
 import java.net.NetworkInterface
+import java.util.concurrent.TimeUnit
+import android.app.ActivityManager
+import android.view.accessibility.AccessibilityManager
+import android.accessibilityservice.AccessibilityServiceInfo
+val feedbackType = AccessibilityServiceInfo.FEEDBACK_ALL_MASK
+
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var serverStatusTextView: TextView
     private val statusUpdateReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
-            val status = intent?.getStringExtra("status") ?: "Local server is not working"
-            serverStatusTextView.text = status
+            updateServerStatus()
         }
     }
 
@@ -47,25 +51,63 @@ class MainActivity : AppCompatActivity() {
         // Обработка нажатия на кнопку "Copy address"
         copyAddressButton.setOnClickListener {
             copyToClipboard(serverAddress)
-            Toast.makeText(this, "Address copied to clipboard", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Done", Toast.LENGTH_SHORT).show()
         }
 
         // Запуск LocalServerService при создании MainActivity
         startService(Intent(this, LocalServerService::class.java))
+
+        // Регистрируем BroadcastReceiver для получения обновлений статуса сервера
+        registerReceiver(statusUpdateReceiver, IntentFilter("com.v1v3r.infolocalserver.STATUS_UPDATE"))
 
         // Обработка нажатия на кнопку "Restart server"
         restartServerButton.setOnClickListener {
             restartServer()
         }
 
-        // Регистрируем BroadcastReceiver для получения обновлений статуса сервера
-        registerReceiver(statusUpdateReceiver, IntentFilter("com.v1v3r.infolocalserver.STATUS_UPDATE"))
+        // Инициализация статуса сервера
+        updateServerStatus()
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Обновляем статус сервера каждый раз при открытии окна
+        updateServerStatus()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         // Отменяем регистрацию BroadcastReceiver
         unregisterReceiver(statusUpdateReceiver)
+    }
+
+    // Метод для обновления статуса сервера
+    private fun updateServerStatus() {
+        when {
+            isAccessibilityServiceRunning(LocalServerAccessibilityService::class.java) -> {
+                serverStatusTextView.text = "Local server is working\nvia accessibility service"
+            }
+            isServiceRunning(LocalServerService::class.java) -> {
+                serverStatusTextView.text = "Local server is working"
+            }
+            else -> {
+                serverStatusTextView.text = "Local server is not working"
+            }
+        }
+    }
+
+    private fun restartServer() {
+        if (isAccessibilityServiceRunning(LocalServerAccessibilityService::class.java)) {
+            // Если сервис доступен, останавливаем и перезапускаем его
+            stopService(Intent(this, LocalServerAccessibilityService::class.java))
+            startService(Intent(this, LocalServerAccessibilityService::class.java))
+        } else {
+            // Иначе перезапускаем LocalServerService
+            stopService(Intent(this, LocalServerService::class.java))
+            startService(Intent(this, LocalServerService::class.java))
+        }
+        updateServerStatus() // Обновляем статус после перезапуска сервера
     }
 
     private fun getLocalIpAddress(): String {
@@ -93,34 +135,25 @@ class MainActivity : AppCompatActivity() {
         clipboardManager.setPrimaryClip(clipData)
     }
 
-    private fun restartServer() {
-        // Остановка LocalServerAccessibilityService, если он запущен
-        try {
-            val accessibilityServiceIntent = Intent(this, LocalServerAccessibilityService::class.java)
-            stopService(accessibilityServiceIntent)
-        } catch (e: Exception) {
-            Log.e("MainActivity", "LocalServerAccessibilityService is not running")
-        }
+    private fun isAccessibilityServiceRunning(serviceClass: Class<*>): Boolean {
+        val am = getSystemService(Context.ACCESSIBILITY_SERVICE) as AccessibilityManager
+        val enabledServices = am.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_ALL_MASK)
 
-        // Запуск LocalServerAccessibilityService
-        if (isAccessibilityServiceRunning(LocalServerAccessibilityService::class.java)) {
-            startService(Intent(this, LocalServerAccessibilityService::class.java))
-            Toast.makeText(this, "LocalServerAccessibilityService restarted", Toast.LENGTH_SHORT).show()
-        } else {
-            // Если LocalServerAccessibilityService не запущен, то запускаем LocalServerService
-            startService(Intent(this, LocalServerService::class.java))
-            Toast.makeText(this, "LocalServerService restarted", Toast.LENGTH_SHORT).show()
+        for (serviceInfo in enabledServices) {
+            if (serviceInfo.resolveInfo.serviceInfo.packageName == packageName &&
+                serviceInfo.resolveInfo.serviceInfo.name == serviceClass.name) {
+                return true
+            }
         }
+        return false
     }
 
-    private fun isAccessibilityServiceRunning(serviceClass: Class<out AccessibilityService>): Boolean {
-        val am = getSystemService(Context.ACCESSIBILITY_SERVICE) as AccessibilityManager
-        val enabledServices = Settings.Secure.getString(contentResolver, Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES)
+    private fun isServiceRunning(serviceClass: Class<*>): Boolean {
+        val activityManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        val services = activityManager.getRunningServices(Int.MAX_VALUE)
 
-        val colonSplitter = enabledServices.split(":").toList()
-
-        for (componentName in colonSplitter) {
-            if (componentName.equals(serviceClass.name, ignoreCase = true)) {
+        for (service in services) {
+            if (serviceClass.name == service.service.className) {
                 return true
             }
         }
